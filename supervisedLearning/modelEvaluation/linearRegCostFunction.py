b040b8f88264541d971d0936c5f604ae99c59d50
robotenique <julianogarcia_1997@hotmail.com>
2017-10-17 15:54:02 -0200

    finish vectorized impl of linear regression with gradient

--- supervisedLearning/modelEvaluation/linearRegCostFunction.py
+++ supervisedLearning/modelEvaluation/linearRegCostFunction.py
@@ -9,13 +9,8 @@ def linearRegCostFunction(X, y, theta, Lambda):
     # Initialize some useful values
     m = y.size  # number of training examples
     grad = np.zeros(theta.shape)
-    int_err = np.sum(theta*X, axis=1) - y # h(x) - y
-    J = np.sum((int_err)**2)/(2*m) + Lambda*theta[1:]**2/(2*m)
-    print(np.sum(int_err))
-    print(theta.T[0]*np.sum(int_err)/m)
-    grad[0] = theta.T[0]*np.sum(int_err)/m
-    1*-183.63
-    grad[1] = (theta.T[1:]*np.sum(int_err))/m + theta.T[1:]*Lambda/m
-    print(grad[1])
-    exit()
+    err = np.sum(theta*X, axis=1) - y # Matrix h(X) - y
+    J = np.sum((err)**2)/(2*m) + Lambda*theta[1:]**2/(2*m)
+    grad = np.sum((err.reshape(err.size, 1)*X).T, axis=1)/m + theta.T*Lambda/m
+    grad[0] -= theta[0]*Lambda/m
     return J, grad
